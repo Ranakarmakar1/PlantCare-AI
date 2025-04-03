297e97fbf919ec1c99f91e2347961adcb01bdc1b
ranakarmakar1 <ranakarmakar43@gmail.com>
2025-04-03 12:42:53 +0530

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github, Mail } from 'lucide-react';
+import {  Mail } from 'lucide-react';
 
 function Footer() {
   return (
@@ -13,9 +13,7 @@ function Footer() {
           <a href="mailto:contact@plantcareai.com" className="text-gray-300 hover:text-white">
             <Mail className="h-6 w-6" />
           </a>
-          <a href="https://github.com/plantcareai" className="text-gray-300 hover:text-white">
-            <Github className="h-6 w-6" />
-          </a>
+          
         </div>
         <p className="text-gray-400 text-xs mt-4">&copy; {new Date().getFullYear()} PlantCare AI. All rights reserved.</p>
       </div>
